715aee616d1c0283484cc16bb7ca87ea1e0935d7
tm1990 <haavardpc@hotmail.com>
2014-08-12 16:51:53 +0200

    Finaly a working gradle system...

--- src/main/java/CrystallineMagic/Utils/Ref.java
+++ src/main/java/CrystallineMagic/Utils/Ref.java
@@ -5,7 +5,7 @@ public class Ref {
 
     public static final String ModId = "CrystMagic";
     public static final String ModName = "CrystallineMagic";
-    public static final String ModVersion = "1.0.0";
+    public static final String ModVersion = "@VERSION@";
     public static final String ModChannel = "Cryst";
 }
 
